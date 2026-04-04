cb470dfba8d2874ebccbdaff7e38c1b69cfc2396
MASAMICHI IIZUMI <m.iizumi@miosync.email>
2026-04-04 18:34:44 +0900

    Update __init__.py

--- getter_one/__init__.py
+++ getter_one/__init__.py
@@ -33,7 +33,7 @@ from typing import Any
 # Version
 # ===============================
 
-__version__ = "0.1.9"
+__version__ = "0.2.0"
 __author__ = "Masamichi Iizumi & Tamaki"
 
 # ===============================
